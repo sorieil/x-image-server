74c97973fc033795b3e282daa153029b180ea737
Jinhwan Kim <pleasantlife@xsync.co>
2020-07-29 11:53:58 +0900

    200729 ETC : pipeline setting 변경

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+TEMP_DIR="server-image-api-new"
+DIR="server-image-api"
+if [ -d "$TEMP_DIR" ]; then
+    rm -rf $TEMP_DIR
+    echo "Delete " + $TEMP_DIR
+fi
+eval `ssh-agent -s`
+ssh-add ~/.ssh/bitbucket_rsa
+git clone git@bitbucket.org:xsync_development/server-image-api.git $TEMP_DIR
+echo "Clone success"
+cd $TEMP_DIR
+git checkout -t origin/master
+echo "Success git pull"
+yarn --production=false
+echo "Success node_modules"
+#npx tsc You have to complie at this point, but I'm temporarily compiling locally for server performonce issues.
+echo "Success Build"
+cd ~
+rm -rf $DIR
+mv $TEMP_DIR $DIR
+cd $DIR
+pm2 start ecosystem.config.js --env production
+echo "Success Start"
+echo "Successfully master deploy"
